7f09e5675dcd6b1b0276f51ce03c97fcaa07ae7e
Lakshay Bhardwaj <91718893+lakshay1121@users.noreply.github.com>
2023-02-20 09:07:55 -0800

    Update NavigationHandler.js

--- navigation/NavigationHandler.js
+++ navigation/NavigationHandler.js
@@ -1,16 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Screen1 from "../screens/Screen1";
 import Screen2 from "../screens/Screen2";
+import SignupPage from "../screens/SignupPage";
+import LoginPage from "../screens/LoginPage";
+import Screen4 from "../screens/Screen4";
 
 const NavigationHandler = () => {
-
-    const Stack = createNativeStackNavigator();
+  const Stack = createNativeStackNavigator();
 
   return (
-
     <NavigationContainer style={styles.container}>
       <Stack.Navigator initialRouteName="Screen1">
         <Stack.Screen
@@ -23,12 +24,29 @@ const NavigationHandler = () => {
           component={Screen2}
           options={{ headerShown: false }}
         />
+
+        <Stack.Screen
+          name="LoginPage"
+          component={LoginPage}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="SignupPage"
+          component={SignupPage}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Screen4"
+          component={Screen4}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
-      
     </NavigationContainer>
-  )
-}
+  );
+};
 
-export default NavigationHandler
+export default NavigationHandler;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
